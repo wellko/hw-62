935d3f03e66c6f3fda0d7d39b60326ab98ffee7a
Kwon Valerii <wellko24@gmail.com>
2022-11-28 17:13:45 +0600

    added Home.tsx

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='container'>
+             <h1 className='text-center'> Welcome </h1>
+            <h2 className='text-center'> There is some information about me:</h2>
+            <p>Name: Kwon Valerii</p>
+            <p>Date of birth: 26.11.1993</p>
+            <p>Education: Student of Attractor School</p>
+        </div>
+    );
+};
+
+export default Home;
