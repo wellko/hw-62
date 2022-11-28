f815ae1b696aff588f3ca2cb1e0847a245126623
Kwon Valerii <wellko24@gmail.com>
2022-11-28 17:14:23 +0600

    added NavBar.tsx with Navlinks

---
+++ src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid">
+                <span className="navbar-brand">Kwon</span>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <NavLink to='/' className="nav-link">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to='/education' className="nav-link">Education</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to='/contacts' className="nav-link">Contacts</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to='portfolio' className="nav-link">Portfolio</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
---
+++ src/Components/Portfolio/Portfolio.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div className='container'>
+            Portfolio page
+        </div>
+    );
+};
+
+export default Portfolio;
